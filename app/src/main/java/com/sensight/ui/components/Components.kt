f05c288b08e34836b2033bfb78871c2c75ec7853
imraklr <44721620+imraklr@users.noreply.github.com>
2023-09-26 00:40:48 +0530

    Long press feature with haptic enabled on sensor card

--- app/src/main/java/com/sensight/ui/components/Components.kt
+++ app/src/main/java/com/sensight/ui/components/Components.kt
@@ -2,7 +2,9 @@
 
 package com.sensight.ui.components
 
+import android.content.Context
 import android.hardware.Sensor
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
@@ -18,28 +20,44 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.Spring
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalHapticFeedback
 import com.sensight.data.repository.getCommonPropertiesAmongSensorsForComponentInfoComposable
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SensorCard(
     sensor: Sensor,
+    context: Context = LocalContext.current,
     modifier: Modifier = Modifier
 ) {
     Spacer(modifier = Modifier.padding(top = 4.dp))
     var expanded by remember { mutableStateOf(false) }
+    val haptics = LocalHapticFeedback.current
     Card(
         modifier = Modifier
             .padding(top = 4.dp)
-            .clickable {
-                expanded = !expanded
-            }
+            .combinedClickable(
+                onClick = {
+                    expanded = !expanded
+                },
+                onLongClick = {
+                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                    /*
+                     * Calls to other activity/composable goes here
+                    */
+                }
+            )
     ) {
         Column(
             modifier = Modifier
